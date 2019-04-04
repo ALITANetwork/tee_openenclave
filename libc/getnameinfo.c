b19d664af74fd7e22a00f6a1fc5befd268b38a2e
Mike Brasher <mikbras@microsoft.com>
2019-04-04 17:29:09 -0500

    add getnameinfo.c

---
+++ libc/getnameinfo.c
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include <openenclave/corelibc/netdb.h>
+
+int getnameinfo(
+    const struct sockaddr* sa,
+    socklen_t salen,
+    char* host,
+    socklen_t hostlen,
+    char* serv,
+    socklen_t servlen,
+    int flags)
+{
+    return oe_getnameinfo(
+        (const struct oe_sockaddr) * sa,
+        salen,
+        host,
+        hostlen,
+        serv,
+        servlen,
+        flags);
+}
