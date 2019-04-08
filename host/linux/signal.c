4244fd098a17415e898ad5790352380ea4b98cd7
Bruce Campbell <brcampbe@microsoft.com>
2019-04-08 21:46:45 +0000

    oe side of signal with tests

---
+++ host/linux/signal.c
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+// clang-format off
+#include <openenclave/host.h>
+#include <sys/signal.h>
+#include <errno.h>
+#include "oe_u.h"
+// clang-format on
+
+// Poll uses much of the infrastructure from epoll.
+
+int oe_signal_kill(int pid, int signum, int* err)
+
+{
+    int retval = -1;
+
+    *err = 0;
+
+    retval = kill(pid, signum);
+    if (retval < 0)
+    {
+        err = errno;
+    }
+
+done:
+    return retval;
+}
